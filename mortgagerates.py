6f8f54e8790ab4089344ee667d3e9977a92062a5
tendress <anthony.endress@gmail.com>
2023-07-26 17:56:20 -0400

    Update mortgagerates.py

--- mortgagerates.py
+++ mortgagerates.py
@@ -12,7 +12,7 @@ API_Key = '5e8ed80210b838d7123b16e9e8faf111'
 fred = Fred(api_key=API_Key)
  
 ######### Get the Rates from FRED ##########
-FEDFUNDSStats = fred.get_series(series_id='FEDFUNDS', 'PRIME')
+FEDFUNDSStats = fred.get_series(series_id='FEDFUNDS')
 PrimeStats = fred.get_series(series_id='PRIME')
 
 MORTGAGE30USStats = fred.get_series(series_id='MORTGAGE30US')
